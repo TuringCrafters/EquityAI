c85b67fedf5d0073a1597788c28cec507c31cbc1
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-03 13:48:17 +0100

    init commit

---
+++ server/src/test/java/ai/equity/salt/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.equity.salt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
