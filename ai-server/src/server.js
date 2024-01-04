9e9c897571c2e42e7d16a36e33de5d62bf6685b8
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-04 14:51:40 +0100

    added cors

--- ai-server/src/server.js
+++ ai-server/src/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
+
+app.use(cors({
+    origin: 'http://localhost:3001' 
+  }));
 
 app.use(express.json());
 
 app.post('/ai', (req, res) => {
     const userInput = req.body.text;
-    const processedText = userInput.toUpperCase();
-    res.json({ processedText });
+    const response = userInput.toUpperCase();
+    res.json({ response });
 });
 
 app.listen(port, () => {
