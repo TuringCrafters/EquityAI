1adca801324559a16bedd8d468190b8090b6cbb2
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-04 15:55:11 +0100

    exporting the function and returning the received text

--- ai-server/src/index.js
+++ ai-server/src/index.js
@@ -3,15 +3,14 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_PRO_API_KEY);
 
-async function run() {
+async function sendPromptToAi(prompt) {
     const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-  
-    const prompt = "Write a story about a magic backpack."
   
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
-    console.log(text);
+    return text;
   }
 
-  run();
+  module.exports = { sendPromptToAi };
+
