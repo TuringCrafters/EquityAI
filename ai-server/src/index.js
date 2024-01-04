3475534dc938613c1b52b8f0913feb8205812928
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-04 13:25:04 +0100

    Connected to generative ai api

--- ai-server/src/index.js
+++ ai-server/src/index.js
@@ -1,1 +1,19 @@
-console.log(`Hello World!`);
+require('dotenv').config({ path: './.env.local' });
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_PRO_API_KEY);
+
+async function run() {
+    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  
+    const prompt = "Write a story about a magic backpack."
+  
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+  }
+
+  console.log(process.env.GEMINI_PRO_API_KEY);
+
+  run();
