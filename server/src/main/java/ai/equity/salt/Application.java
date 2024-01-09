4f9fc0be58f87c6a241301c6c00995ec08f1ffdb
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2024-01-09 10:45:11 +0100

    add test to check connection to openai api

--- server/src/main/java/ai/equity/salt/Application.java
+++ server/src/main/java/ai/equity/salt/Application.java
@@ -2,7 +2,9 @@ package ai.equity.salt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class Application {
 
--- server/src/main/java/ai/equity/salt/openai/Lang.java
+++ server/src/main/java/ai/equity/salt/openai/Lang.java
@@ -1,5 +1,10 @@
 package ai.equity.salt.openai;
 
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiLanguageModel;
+import dev.langchain4j.model.language.LanguageModel;
+
+
 public class Lang {
     LanguageModel model = OpenAiLanguageModel.builder()
             .apiKey(System.getenv("OPENAI_API_KEY"))
