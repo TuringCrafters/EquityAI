4b3ab743c904a0bb53aabfeae44bb5b1db5dd9eb
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-10 16:24:45 +0100

    send prompt to ai and receive response using useQuery

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,48 +1,50 @@
 "use client";
 
-import axios from "axios";
-import { useState } from "react";
+import { useState } from 'react';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+
 
 export default function Home() {
-  const [prompt, setPrompt] = useState("");
-  const [aiResponse, setAiResponse] = useState("");
+  const [prompt, setPrompt] = useState('');
+
+  async function fetchAiResponse () {
+    const response = await axios.post('http://localhost:8080/api/v1/ai', { prompt });
+    return response.data;
+  };
+
+  const { data: aiResponse, isLoading, refetch } = useQuery({
+    queryKey: ['aiResponse', { prompt }],
+    queryFn: fetchAiResponse,
+    enabled: false
+  });
+
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
     setPrompt(e.target.value);
   };
 
-  async function sendPromptToServer(prompt: string) {
-    try {
-      const response = await axios.post("http://localhost:8080/api/v1/ai", {
-        prompt,
-      });
-      setAiResponse(response.data);
-      console.log(aiResponse);
-    } catch (error) {
-      console.error("Error occurr during fetching", error);
-    }
-  }
-
-  const handleSubmit = () => {
-    sendPromptToServer(prompt);
-    setPrompt("");
-    console.log("sent info");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); 
+    refetch();
   };
 
   return (
     <main>
       <h4>Ask something to AI</h4>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           onChange={handleInputChange}
           type="text"
           value={prompt}
           placeholder="Ask me!"
         />
-        <button onClick={handleSubmit}>Ask</button>
+        <button type="submit">Ask</button>
       </form>
-      <h5>{aiResponse}</h5>
+      {aiResponse && 
+      isLoading ? <p>Loading...</p> : <h5>{aiResponse}</h5>}
     </main>
   );
 }
+
