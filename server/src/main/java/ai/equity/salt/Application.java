001aeb32299d0480a93dccc4b79f210be7698ee2
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2024-01-11 14:03:49 +0100

    remove cross-origin annotation

--- server/src/main/java/ai/equity/salt/Application.java
+++ server/src/main/java/ai/equity/salt/Application.java
@@ -3,6 +3,7 @@ package ai.equity.salt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
@@ -11,5 +12,9 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+
+
+
 
 }
