65d3185d49bcf652b9e5e9f88c70c5bc8b1f9b5f
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-11 14:41:04 +0100

    delete annotation

--- server/src/test/java/ai/equity/salt/TestApplication.java
+++ server/src/test/java/ai/equity/salt/TestApplication.java
@@ -2,10 +2,7 @@ package ai.equity.salt;
 
 import ai.equity.salt.config.LocalDevTestcontainersConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.context.TestConfiguration;
 
-
-@TestConfiguration(proxyBeanMethods = false)
 public class TestApplication {
 
 	public static void main(String[] args) {
