032ae84d03776766731c1daabbac4aec3bf017b6
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2024-01-11 14:04:46 +0100

    create method to allow only specific origins

--- server/src/main/java/ai/equity/salt/openai/config/WebConfig.java
+++ server/src/main/java/ai/equity/salt/openai/config/WebConfig.java
@@ -1,13 +1,29 @@
 package ai.equity.salt.openai.config;
 
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig{
 
+    @Value("${website.base-url.frontend}")
+    private String allowedApi;
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings (@NotNull CorsRegistry registry){
+                System.out.println(allowedApi);
+                registry.addMapping("/api/**")
+                        .allowedOrigins(allowedApi)
+                        .allowedMethods("GET", "POST", "DELETE", "PUT");
+            }
+        };
+    }
 
 }
