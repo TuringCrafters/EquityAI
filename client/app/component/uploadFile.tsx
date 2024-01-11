98a3bc8c6d6b33df9f16c4ce711e889b433dc63e
AnneMariel95 <a.mariel.recio@gmail.com>
2024-01-11 15:30:52 +0100

    create upload file logic

---
+++ client/app/component/uploadFile.tsx
@@ -0,0 +1,56 @@
+"use client";
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
+
+interface ClothingFormProps {
+  id?: string;
+}
+
+const UploadFile = () => {
+  const [file, setFile] = useState<Blob | null>(null);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const [isSuccessVisible, setIsSuccessVisible] = useState(false);
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setFile(event.target.files[0]);
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+
+    if (file) {
+      formData.append("image", file);
+    }
+
+    setIsSuccessVisible(true);
+    setTimeout(() => {
+      setIsSuccessVisible(false);
+    }, 2000);
+  };
+
+  return (
+    <div className="w-full max-w-md">
+      <form onSubmit={handleSubmit} className="w-full max-w-m px-1 py-6">
+        <div>
+          <input
+            type="file"
+            ref={fileInputRef}
+            onChange={handleFileChange}
+            className="block w-full text-sm text-gray-500 
+                       file:bg-primary-content file:btn btn-accent file:rounded-full file:border-0 file:text-sm file:font-normal file:text-secondary-content "
+          />
+        </div>
+        {isSuccessVisible && (
+          <div className="text-green-600 text-center">Successful!</div>
+        )}
+        <button onSubmit={() => handleSubmit}>Upload</button>
+      </form>
+    </div>
+  );
+};
+
+export default UploadFile;
