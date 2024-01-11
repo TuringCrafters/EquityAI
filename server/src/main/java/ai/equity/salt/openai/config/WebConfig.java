8a263303773b625150c58eb7f1fbfdb57e4fc816
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2024-01-11 13:31:19 +0100

    create webconfig class

---
+++ server/src/main/java/ai/equity/salt/openai/config/WebConfig.java
@@ -0,0 +1,13 @@
+package ai.equity.salt.openai.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+
+
+}
