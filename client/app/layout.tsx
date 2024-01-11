7fbe72949e581e59ffdd3bd4d24afb53f84c0e88
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2024-01-11 08:31:34 +0100

    refactor page to fit full height

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" className='h-full'>
+      <body className={inter.className} >
         <Providers>{children}</Providers>
       </body>
     </html>
