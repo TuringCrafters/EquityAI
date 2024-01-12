ebdfb52db0445ec266dc9349d4f9b9c748430ccb
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-12 11:31:09 +0100

    remove unused import

--- server/src/main/java/ai/equity/salt/config/WebConfig.java
+++ server/src/main/java/ai/equity/salt/config/WebConfig.java
@@ -2,7 +2,6 @@ package ai.equity.salt.config;
 
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
