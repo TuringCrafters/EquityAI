368dbda81d1bf81767141aa622eb1e237dbc0b24
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-15 17:11:24 +0100

    Updated response entity

--- server/src/main/java/ai/equity/salt/openai/controller/dto/EquityAiResponse.java
+++ server/src/main/java/ai/equity/salt/openai/controller/dto/EquityAiResponse.java
@@ -1,7 +1,10 @@
 package ai.equity.salt.openai.controller.dto;
 
+import java.util.Set;
+
 public record EquityAiResponse(
         String response,
+        Set<String> uniqueJobTitles,
         EquityAiDatapointExperienceResponse datapointExperience
 ) {
 }
--- server/src/main/java/ai/equity/salt/openai/service/EquityAiService.java
+++ server/src/main/java/ai/equity/salt/openai/service/EquityAiService.java
@@ -79,6 +79,4 @@ public class EquityAiService {
             throw new RuntimeException(e);
         }
     }
-
-
 }
