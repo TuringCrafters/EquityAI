548e09706f520b88e9b8a12ad55d5a711b9af2d5
Ibrahim <Ibrahim@ebs.net.pk>
2024-01-15 19:22:36 +0100

    Fix import statement in page.tsx

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,37 +2,8 @@
 import { useState } from "react";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
-import UploadFile from "@/components/upload/UploadFile";
+import UploadFile from "@/components/upload/UploadFile"
 
 export default function Home() {
-  const [prompt, setPrompt] = useState("");
-
-  async function fetchAiResponse() {
-    const response = await axios.post(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/ai`,
-      { prompt }
-    );
-    return response.data;
-  }
-
-  const {
-    data: aiResponse,
-    isLoading,
-    refetch,
-  } = useQuery({
-    queryKey: ["aiResponse", { prompt }],
-    queryFn: fetchAiResponse,
-    enabled: false,
-  });
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPrompt(e.target.value);
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    refetch();
-  };
-
   return <UploadFile/>;
 }
