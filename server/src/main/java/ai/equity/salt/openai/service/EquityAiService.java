8c0bc3b1f963b302f8dd751c73cf5f2634fff8f8
Ibrahim <Ibrahim@ebs.net.pk>
2024-01-15 13:36:00 +0100

    improved the system message

--- server/src/main/java/ai/equity/salt/openai/service/EquityAiService.java
+++ server/src/main/java/ai/equity/salt/openai/service/EquityAiService.java
@@ -20,20 +20,15 @@ import java.util.stream.Collectors;
 @Slf4j
 public class EquityAiService {
 
+    private static final String SYSTEM_MESSAGE = """
+            Analyze the provided dataset, which includes positions, salaries, age, and locality to identify significant discrepancies. Focus on:
+            -Any unexpected patterns or outliers.
+            -Any discrepancies within the data.
+            -Point out if similar positions, years of experience and locality with similar job complexity have different salaries
+            Highlight and explain any noteworthy discrepancies beyond these areas. Ensure your analysis is thorough and accounts for possible contributing factors to these disparities.
+            Provide the result in plain english. Make sure the answer is in layman's terms""";
     private final OpenAiModelFactory openAiModelFactory;
-
     private final JpaEquityAiRepo repository;
-
-    private final String SYSTEM_MESSAGE = """
-            Analyze the provided dataset, which includes positions, salaries, age, and locality, to identify significant discrepancies. Focus on:
-
-            Salary differences within the same positions.
-            Salary distribution variations across localities.
-            Age-related disparities in salaries and positions.
-            Any unexpected patterns or outliers.
-
-            Use statistical measures and visualizations to support your findings. Highlight and explain any noteworthy discrepancies beyond these areas. Ensure your analysis is thorough and accounts for possible contributing factors to these disparities.
-            Provide the result in plain english.""";
 
     public String getAiResponse(String prompt) {
 
