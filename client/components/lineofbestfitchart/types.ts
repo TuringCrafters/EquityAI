6eb622f84a75c10cde0ddc23c6317a58f8d485de
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2024-01-16 14:37:31 +0100

    created types for linear chart component

---
+++ client/components/lineofbestfitchart/types.ts
@@ -0,0 +1,12 @@
+import { experience_details } from "@/app/analysis/page";
+
+export type LinearChartGraphProps = {
+  data: experience_details[];
+};
+
+export type TransformedExperienceDetails = {
+    yearsOfExperience: number;
+  salary_average: number;
+  salary_above_average: number;
+  salary_below_average: number;
+};
