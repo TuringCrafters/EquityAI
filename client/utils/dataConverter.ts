914ff9ca385f91e061906a54ce39bb2b04b9ff5a
Ibrahim <Ibrahim@ebs.net.pk>
2024-01-16 15:27:19 +0100

    Update salary values to be in thousands

--- client/utils/dataConverter.ts
+++ client/utils/dataConverter.ts
@@ -5,17 +5,17 @@ import { TransformedExperienceDetails } from "@/components/lineofbestfitchart/ty
 export function transformLocationDetails(originalDetails: location_details): TransformedLocationDetails {
     return {
       location: originalDetails.location,
-      salary_average: originalDetails.salary.average,
-      salary_above_average: originalDetails.salary.above_average,
-      salary_below_average: originalDetails.salary.below_average,
+      salary_average: originalDetails.salary.average/1000,
+      salary_above_average: originalDetails.salary.above_average/1000,
+      salary_below_average: originalDetails.salary.below_average/1000,
     };
   }
 
   export function transformExperienceDetails(originalDetails: experience_details): TransformedExperienceDetails {
     return {
       yearsOfExperience: originalDetails.years_of_experience,
-      salary_average: originalDetails.salary.average,
-      salary_above_average: originalDetails.salary.above_average,
-      salary_below_average: originalDetails.salary.below_average,
+      salary_average: originalDetails.salary.average/1000,
+      salary_above_average: originalDetails.salary.above_average/1000,
+      salary_below_average: originalDetails.salary.below_average/1000,
     };
   }
