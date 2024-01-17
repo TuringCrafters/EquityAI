0f244b9051f5a7af108b82c1892da90c867dd794
AnneMariel95 <a.mariel.recio@gmail.com>
2024-01-17 10:53:16 +0100

    rename dto and update name

--- server/src/main/java/ai/equity/salt/openai/controller/dto/EquityAiDatapointExperienceResponse.java
+++
@@ -1,9 +0,0 @@
-package ai.equity.salt.openai.controller.dto;
-
-import java.util.List;
-
-public record EquityAiDatapointExperienceResponse(
-        String jobTitle,
-        List<EquityAiYearsOfExperience> yearsOfExperience
-) {
-}
