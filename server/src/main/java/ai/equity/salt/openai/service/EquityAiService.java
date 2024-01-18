7a34b5d6e821a9a5808336e7a1ba22433e876e6b
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2024-01-18 15:26:19 +0100

    extract methods to utils file

--- server/src/main/java/ai/equity/salt/openai/service/EquityAiService.java
+++ server/src/main/java/ai/equity/salt/openai/service/EquityAiService.java
@@ -4,7 +4,6 @@ import ai.equity.salt.openai.controller.dto.*;
 import ai.equity.salt.openai.model.EquityAi;
 import ai.equity.salt.openai.model.OpenAiModelFactory;
 import ai.equity.salt.openai.repository.JpaEquityAiRepo;
-import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,10 +11,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import static ai.equity.salt.openai.utils.FileReader.readCSV;
+import static ai.equity.salt.openai.utils.DataAnalysis.*;
 
 @Service
 @RequiredArgsConstructor
@@ -56,30 +57,6 @@ public class EquityAiService {
         return new EquityAiResponse("null", uniqueJobTitles, mostCommonJob, experienceDataPoints, locationDataPoints);
     }
 
-    private static List<JobDataSet> readCSV(InputStream inputStream) throws IOException, CsvValidationException {
-        List<JobDataSet> jobDataList = new ArrayList<>();
-
-        try (CSVReader csvReader = new CSVReader((new InputStreamReader(inputStream, StandardCharsets.UTF_8)))) {
-            csvReader.readNext();
-
-            String[] nextRecord;
-            while ((nextRecord = csvReader.readNext()) != null) {
-                if (nextRecord.length < 5) {
-                    continue;
-                }
-                JobDataSet jobData = new JobDataSet();
-                jobData.setPosition(nextRecord[0]);
-                jobData.setSalary(Double.parseDouble(nextRecord[1]));
-                jobData.setExperience(Integer.parseInt(nextRecord[2]));
-                jobData.setAge(Integer.parseInt(nextRecord[3]));
-                jobData.setLocality(nextRecord[4]);
-
-                jobDataList.add(jobData);
-            }
-        }
-        return jobDataList;
-    }
-
     private static String createPrompt(List<JobDataSet> jobDataList) {
         StringBuilder stringBuilder = new StringBuilder();
 
@@ -112,88 +89,5 @@ public class EquityAiService {
                 .max(Map.Entry.comparingByValue())
                 .map(Map.Entry::getKey)
                 .orElse(null);
-    }
-
-    private List<SalaryByYearsOfExperienceDatapoint> calculateAverageForYearsOfExperience(List<JobDataSet> jobDataList, String mostCommonJob) {
-        Map<Integer, List<Double>> averageSalaryByExperience = jobDataList.stream()
-                .filter(data -> data.getPosition().equals(mostCommonJob))
-                .collect(Collectors.groupingBy(
-                        JobDataSet::getExperience,
-                        Collectors.mapping(JobDataSet::getSalary, Collectors.toList())
-                ));
-
-        return averageSalaryByExperience.entrySet().stream()
-                .map(entry -> {
-                    List<Double> salaries = entry.getValue();
-                    double average = calculateAverage(salaries);
-                    double standardDeviation = calculateStandardDeviation(salaries, average);
-
-                    double aboveAverage = findAboveAverage(salaries, average, standardDeviation);
-                    double belowAverage = findBelowAverage(salaries, average, standardDeviation);
-
-                    return new SalaryByYearsOfExperienceDatapoint(
-                            entry.getKey(),
-                            new SalaryRangeDatapoint(
-                                    (double) Math.round(average * 100) / 100,
-                                    aboveAverage,
-                                    belowAverage
-                            )
-                    );
-                })
-                .toList();
-    }
-
-    private List<SalaryByLocationDatapoint> calculateAverageForLocation(List<JobDataSet> jobDataList, String mostCommonJob) {
-        Map<String, List<Double>> averageSalaryByLocation = jobDataList.stream()
-                .filter(data -> data.getPosition().equals(mostCommonJob))
-                .collect(Collectors.groupingBy(
-                        JobDataSet::getLocality,
-                        Collectors.mapping(JobDataSet::getSalary, Collectors.toList())
-                ));
-
-        return averageSalaryByLocation.entrySet().stream()
-                .map(entry -> {
-                    List<Double> salaries = entry.getValue();
-                    double average = calculateAverage(salaries);
-                    double standardDeviation = calculateStandardDeviation(salaries, average);
-
-                    double aboveAverage = findAboveAverage(salaries, average, standardDeviation);
-                    double belowAverage = findBelowAverage(salaries, average, standardDeviation);
-
-                    return new SalaryByLocationDatapoint(
-                            entry.getKey(),
-                            new SalaryRangeDatapoint(
-                                    (double) Math.round(average * 100) / 100,
-                                    aboveAverage,
-                                    belowAverage
-                            )
-                    );
-                })
-                .toList();
-    }
-
-    private double calculateAverage(List<Double> salaries) {
-        return salaries.stream().mapToDouble(Double::doubleValue).average().orElse(0);
-    }
-
-    private double calculateStandardDeviation(List<Double> salaries, double mean) {
-        double sumOfSquares = salaries.stream()
-                .mapToDouble(salary -> Math.pow(salary - mean, 2))
-                .sum();
-        return Math.sqrt(sumOfSquares / salaries.size());
-    }
-
-    private double findAboveAverage(List<Double> salaries, double average, double standardDeviation) {
-        return salaries.stream()
-                .filter(salary -> salary > average + standardDeviation)
-                .max(Double::compare)
-                .orElse(average);
-    }
-
-    private double findBelowAverage(List<Double> salaries, double average, double standardDeviation) {
-        return salaries.stream()
-                .filter(salary -> salary < average - standardDeviation)
-                .min(Double::compare)
-                .orElse(average);
     }
 }
