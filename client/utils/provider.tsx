dd92db495b048d6aa4b6f5e4e00ae9af6543fb92
AnneMariel95 <a.mariel.recio@gmail.com>
2024-01-18 11:41:06 +0100

    create DataContextType interface

--- client/utils/provider.tsx
+++ client/utils/provider.tsx
@@ -3,16 +3,23 @@ import { Analysis } from "@/app/analysis/types";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createContext, useState } from "react";
 
+interface DataContextType {
+  data: Analysis | null, 
+  setData: React.Dispatch<React.SetStateAction<Analysis | null>>
+}
 
 
-export const DataContext = createContext<Analysis | null>(null);
+export const DataContext = createContext<DataContextType>({
+  data: null,
+  setData: () => {}
+});
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient()
   const [data, setData] = useState<Analysis | null>(null);
   return (
     <QueryClientProvider client={queryClient}>
-      <DataContext.Provider value={data}>
+      <DataContext.Provider value={{data, setData}}>
         {children}
       </DataContext.Provider>
     </QueryClientProvider>
