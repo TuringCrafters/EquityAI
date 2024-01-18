dd185f5f93a97c96700b36d95614f6a3bb6a32e7
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-18 10:53:54 +0100

    Add create context to provider

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,6 @@
 "use client";
 import UploadFile from "@/components/upload/UploadFile"
-import { DataContext } from "@/utils/provider";
-import { useContext } from "react";
 
 export default function Home() {
-
-  const data = useContext(DataContext);
-  console.log(data);
   return <UploadFile/>;
 }
--- client/utils/provider.tsx
+++ client/utils/provider.tsx
@@ -1,15 +1,15 @@
 "use client";
+import { Analysis } from "@/app/analysis/types";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createContext, useState } from "react";
 
 
 
-export const DataContext = createContext("");
+export const DataContext = createContext<Analysis | null>(null);
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient()
-
-  const [data, setData] = useState("Ello");
+  const [data, setData] = useState<Analysis | null>(null);
   return (
     <QueryClientProvider client={queryClient}>
       <DataContext.Provider value={data}>
