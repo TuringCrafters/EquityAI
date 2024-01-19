74ef777babaa24914c2c489e3d7dd5cc632f713c
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2024-01-19 11:46:43 +0100

    refacctor types analysis to accept product recommendation information

--- client/types/Analysis.ts
+++ client/types/Analysis.ts
@@ -3,7 +3,8 @@ import { LocationDetails } from "./LocationDetails";
 
 export type Analysis = {
     response: string;
-    job_title: string
+    job_title: string;
+    product_recommendation: string;
     location_details: LocationDetails[];
     experience_details: ExperienceDetails[];
   }
