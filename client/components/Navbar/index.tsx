f5e11d84b15cd28ea40422f664287e416cc90cb8
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-01-19 13:23:42 +0100

    adding shadow do companies name

--- client/components/Navbar/index.tsx
+++ client/components/Navbar/index.tsx
@@ -1,6 +1,6 @@
 "use client"
 export default function NavBar() {
   return <nav>
-    <h1 className="text-2xl font-semibold ml-20 mt-5 tracking-tighter text-white">company.</h1>
+    <h1 className="text-2xl font-semibold ml-20 mt-5 tracking-tighter text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">company.</h1>
   </nav>;
 }
