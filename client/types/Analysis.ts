91ac7895b6d754710634790a2f6f0e4eb7244991
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2024-01-19 11:45:31 +0100

    refactor analysis type to accept most common job_tittle

--- client/types/Analysis.ts
+++ client/types/Analysis.ts
@@ -3,6 +3,7 @@ import { LocationDetails } from "./LocationDetails";
 
 export type Analysis = {
     response: string;
+    job_title: string
     location_details: LocationDetails[];
     experience_details: ExperienceDetails[];
   }
