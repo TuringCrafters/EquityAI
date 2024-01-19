c8ec33599db634dd5510f3e85d51064689f05455
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2024-01-19 11:06:18 +0100

    add navbar and footer to layout

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,31 +1,33 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Providers from '../utils/provider'
-import { Toaster } from "@/components/ui/toaster"
-import NavBar from '@/components/navbar/NavBar'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Providers from "../services/provider";
+import { Toaster } from "@/components/UI/toaster";
+import NavBar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Equity Ai',
-  description: 'Equity Ai',
-}
+  title: "Equity Ai",
+  description: "Equity Ai",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <Providers>
-    <html lang="en" className='h-full'>
-      <body className={inter.className} >
-        <NavBar/>
-        {children}
-        <Toaster/>
-      </body>
-    </html>
+      <html lang="en" className="h-full">
+        <body className={inter.className}>
+          <NavBar />
+          {children}
+          <Toaster />
+          <Footer />
+        </body>
+      </html>
     </Providers>
-  )
+  );
 }
