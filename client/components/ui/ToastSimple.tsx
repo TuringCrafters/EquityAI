7c9bd2ded58f095b6d55b8a65e905dc8c6e4fdf7
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2024-01-19 11:09:16 +0100

    update folder structure for components

--- client/components/ui/ToastSimple.tsx
+++ client/components/ui/ToastSimple.tsx
@@ -1,21 +1,21 @@
-"use client"
- 
-import { Button } from "@/components/ui/button"
-import { useToast } from "@/components/ui/use-toast"
- 
+"use client";
+
+import { Button } from "@/components/UI/button";
+import { useToast } from "@/components/UI/use-toast";
+
 export function ToastSimple() {
-  const { toast } = useToast()
- 
+  const { toast } = useToast();
+
   return (
     <Button
       variant="outline"
       onClick={() => {
         toast({
           description: "Your message has been sent.",
-        })
+        });
       }}
     >
       Show Toast
     </Button>
-  )
+  );
 }
