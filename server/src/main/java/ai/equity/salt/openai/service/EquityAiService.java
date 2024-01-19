1954283614df55929b6e9c9b95b508eeb1eb25d6
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-19 11:34:39 +0100

    remove prompt messages from service

--- server/src/main/java/ai/equity/salt/openai/service/EquityAiService.java
+++ server/src/main/java/ai/equity/salt/openai/service/EquityAiService.java
@@ -28,14 +28,6 @@ public class EquityAiService {
     private final OpenAiModelFactory openAiModelFactory;
 
     private final JpaEquityAiRepo repository;
-    private static final String SYSTEM_MESSAGE = """
-            Analyze the provided dataset, which includes positions, salaries, age, and locality to identify significant discrepancies. Focus on:
-            -Any unexpected patterns or outliers.
-            -Any discrepancies within the data.
-            -Point out if similar positions, years of experience and locality with similar job complexity have different salaries
-            Highlight and explain any noteworthy discrepancies beyond these areas. Ensure your analysis is thorough and accounts for possible contributing factors to these disparities.
-            Provide the result in plain english. Make sure the answer is in layman's terms""";
-    private static final String DATA_HEADER = "Positions, Salaries, Experience, Age, Locality\n";
 
     public String getAiResponse(String prompt) {
         String response = openAiModelFactory.create().generate(prompt).content();
