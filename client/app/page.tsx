a75be5b090ff6b6a1f5ee6a665b3e585831f544d
AnneMariel95 <a.mariel.recio@gmail.com>
2024-01-19 10:49:20 +0100

    set background for homepage

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import UploadFile from "@/components/upload/UploadFile"
-
 export default function Home() {
-  return <UploadFile/>;
+  return <div className={"bg-cover bg-center bg-no-repeat h-screen w-screen absolute inset-0 -z-10"}
+  style={{ backgroundImage: 'url(https://www.sysarb.se/wp-content/uploads/2023/05/Gradient_final_1-scaled.jpg)' }}
+><UploadFile/></div>;
 }
