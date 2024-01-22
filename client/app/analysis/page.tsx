8d08ebad35a13c66ce36dc16a38a6826faef79ec
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-22 15:57:54 +0100

    add styles for pdf

--- client/app/analysis/page.tsx
+++ client/app/analysis/page.tsx
@@ -5,6 +5,7 @@ import { useReactToPrint } from "react-to-print";
 import { DataContext } from "@/services/provider";
 import LineOfBestFitChart from "@/components/LineChart";
 import { Button } from "../../components/ui/button";
+import "../../styles/AnalysisPagePdfStyles.css"
 
 const StaticBarChartgraph = dynamic(() => import("@/components/BarChart"), {
   ssr: false,
@@ -27,35 +28,31 @@ const AnalysisPage = () => {
         Save as PDF
       </Button>
       <article ref={pageRef}>
-        <h2 className="flex ml-72 pl-6 text-2xl mt-10 mb-5">{data?.job_title}</h2>
+        <h2 className="flex ml-72 pl-6 text-2xl mt-10 mb-5 title-for-print">{data?.job_title}</h2>
         <section className="flex flex-col items-center md:flex-row md:items-start align-middle h-3/6 justify-center my-10">
-          <div className="items-center h-full border-r-2 border-r-#aab0b7">
+          <div className="items-center h-full border-r-2 border-r-#aab0b7 chart-for-print">
             {data?.location_details && (
               <>
-                <h3 className="ml-10 mt-5 font-semibold tracking-tighter">
+                <h3 className="ml-10 mt-5 font-semibold tracking-tighter subtitle-for-print-one">
                   Salary based on Location
                 </h3>
-                <div className="chart-for-print">
                   <StaticBarChartgraph
                     key="barchart"
                     data={data.location_details}
                   />
-                </div>
               </>
             )}
           </div>
-          <div className="items-center  max-h-100">
+          <div className="items-center  max-h-100 chart-for-print">
             {data?.experience_details && (
               <>
-                <h3 className="ml-10 mt-5 mb-10 font-semibold tracking-tighter">
+                <h3 className="ml-10 mt-5 mb-10 font-semibold tracking-tighter subtitle-for-print-two">
                   Salary based on Years of Experience
                 </h3>
-                <div className="chart-for-print">
                   <LineOfBestFitChart
                     key="linechart"
                     data={data.experience_details}
                   />
-                </div>
               </>
             )}
           </div>
