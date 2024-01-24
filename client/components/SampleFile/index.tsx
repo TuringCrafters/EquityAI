dbcc49d13505293e21f6fd3f168a7d023f9896d6
AnneMariel95 <a.mariel.recio@gmail.com>
2024-01-24 15:03:54 +0100

    add download button and remove background

--- client/components/SampleFile/index.tsx
+++ client/components/SampleFile/index.tsx
@@ -5,25 +5,38 @@ const samples = ["Dataset.csv"];
 
 const SampleFileForUpload = () => {
   return (
-    <div className="flex flex-col items-center justify-center pt-[20vh] dark:bg-gray-800">
-      <div className="w-full max-w-md p-6 space-y-4 bg-neutral-50/100 rounded-lg">
-        <h1 className="text-xl font-bold text-center">Try with Sample File:</h1>
-        <ul className="flex flex-col gap-1 list-none">
-          {samples.map((fileName) => {
-            return (
-              <li key={fileName} className="list-none">
-                <Link
-                  href={`/samples/${fileName}`}
-                  target="_blank"
-                  className="text-sm cursor-pointer"
+    <div className="flex flex-col items-center justify-center pt-[20vh] text-neutral-50/100">
+      <h1 className="text-xl font-bold text-center">Try with Sample File:</h1>
+      <ul className="flex flex-col gap-1 list-none">
+        {samples.map((fileName) => {
+          return (
+            <li key={fileName} className="list-none flex items-center">
+              <button
+                className="flex items-center"
+                onClick={() => handleDownload(fileName)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6 mr-2"
                 >
-                  Download {fileName}
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+                  />
+                </svg>
+                <Link href={`/samples/${fileName}`} target="_blank" download>
+                  {fileName}
                 </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
