034a431a2fcae40c96d14e5b9928bd8cd9136524
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-24 16:08:45 +0100

    optimise imports

--- server/src/main/java/ai/equity/salt/openai/controller/EquityAiController.java
+++ server/src/main/java/ai/equity/salt/openai/controller/EquityAiController.java
@@ -9,7 +9,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 import static org.springframework.http.HttpStatus.CREATED;
 
