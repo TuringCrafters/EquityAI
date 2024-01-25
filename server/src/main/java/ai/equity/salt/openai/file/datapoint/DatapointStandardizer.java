59de2be1f5ff9eb7b3b4c1166bb8ed53b1782552
Ibrahim <Ibrahim@ebs.net.pk>
2024-01-25 09:45:41 +0100

    Add DatapointStandardizer implementation

---
+++ server/src/main/java/ai/equity/salt/openai/file/datapoint/DatapointStandardizer.java
@@ -0,0 +1,22 @@
+package ai.equity.salt.openai.file.datapoint;
+
+import ai.equity.salt.openai.model.OpenAiModelFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+
+import static ai.equity.salt.openai.utils.AiPromptData.DATAPOINT_EXTRACTOR_PROMPT;
+
+@Component
+@RequiredArgsConstructor
+public class DatapointStandardizer implements FileDataPointStandardize{
+
+    private final OpenAiModelFactory openAiModelFactory;
+    @Override
+    public List<String> getFileStandardizedDataPoints(List<String> fileDataPoints) {
+        return Collections.singletonList(openAiModelFactory.createDefaultChatModel()
+                .generate(DATAPOINT_EXTRACTOR_PROMPT + fileDataPoints));
+    }
+}
