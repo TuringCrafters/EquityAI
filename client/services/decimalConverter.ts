53507c6ec1d5513ad08551d6d37a4fe0b959056e
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-01-25 09:04:01 +0100

    refactor of decimal converter function

--- client/services/decimalConverter.ts
+++ client/services/decimalConverter.ts
@@ -1,1 +1,1 @@
-export const decimalConverter = (value: number): number => parseFloat((value ).toFixed(2));
+export const decimalConverter = (value: number): number => Math.round(value * 100) / 100;
