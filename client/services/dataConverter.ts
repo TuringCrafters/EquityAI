d680e5e440b6c2c1652688151d35748330f768ca
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-01-25 09:03:32 +0100

    converting data to give null values if the above/bellow salaries match the average salaries

--- client/services/dataConverter.ts
+++ client/services/dataConverter.ts
@@ -10,20 +10,38 @@ export function transformLocationDetails(
 ): TransformedLocationDetails {
   return {
     location: originalDetails.data_value,
-    salary_average: decimalConverter(originalDetails.salary.average/ 1000),
-    salary_above_average: decimalConverter(originalDetails.salary.above_average/ 1000),
-    salary_below_average: decimalConverter(originalDetails.salary.below_average/ 1000),
+    salary_average: decimalConverter(originalDetails.salary.average / 1000),
+    salary_above_average: decimalConverter(
+      originalDetails.salary.above_average / 1000
+    ),
+    salary_below_average: decimalConverter(
+      originalDetails.salary.below_average / 1000
+    ),
   };
 }
 
 export function transformExperienceDetails(
   originalDetails: ExperienceDetails
 ): TransformedExperienceDetails {
+  const salaryAboveAverage =
+    originalDetails.salary.above_average === originalDetails.salary.average
+      ? null
+      : originalDetails.salary.above_average;
+  const salaryBellowAverage =
+    originalDetails.salary.below_average === originalDetails.salary.average
+      ? null
+      : originalDetails.salary.below_average;
   return {
     yearsOfExperience: originalDetails.data_value,
-    salary_average: decimalConverter(originalDetails.salary.average/ 1000),
-    salary_above_average: decimalConverter(originalDetails.salary.above_average/ 1000),
-    salary_below_average: decimalConverter(originalDetails.salary.below_average/ 1000),
+    salary_average: decimalConverter(originalDetails.salary.average / 1000),
+    salary_above_average:
+      salaryAboveAverage !== null
+        ? decimalConverter(salaryAboveAverage / 1000)
+        : salaryAboveAverage,
+    salary_below_average:
+      salaryBellowAverage !== null
+        ? decimalConverter(salaryBellowAverage / 1000)
+        : salaryBellowAverage,
   };
 }
 
@@ -33,8 +51,12 @@ export function convertToPolynomialDataPoints(
   const dataPoints: DataPoint[] = [];
 
   for (const i in sortedDetails) {
-    const yearsOfExperience: number = decimalConverter(sortedDetails[i].data_value);
-    const averageSalary: number = decimalConverter(sortedDetails[i].salary.average);
+    const yearsOfExperience: number = decimalConverter(
+      sortedDetails[i].data_value
+    );
+    const averageSalary: number = decimalConverter(
+      sortedDetails[i].salary.average
+    );
 
     dataPoints.push([yearsOfExperience, averageSalary]);
   }
