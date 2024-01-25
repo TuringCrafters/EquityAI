989660c240b7969bdd74fc6e062e9f65089e89ff
Ibrahim <Ibrahim@ebs.net.pk>
2024-01-25 11:40:13 +0100

    implement calculate age method

--- server/src/main/java/ai/equity/salt/openai/file/datapoint/AgeConverter.java
+++ server/src/main/java/ai/equity/salt/openai/file/datapoint/AgeConverter.java
@@ -1,20 +1,40 @@
 package ai.equity.salt.openai.file.datapoint;
 
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
-@RequiredArgsConstructor
+@Slf4j
 public class AgeConverter {
 
-    public static int calculateAge(String dateOfBirth){
+    private AgeConverter() {
+    }
 
+    public static int calculateAge(String dateOfBirth) {
         int currentYear = LocalDate.now().getYear();
+        List<Integer> fourConsecutiveNumbers = new ArrayList<>();
+        int stringLength = dateOfBirth.length();
 
+        for (int i = 0; i <= stringLength - 4; i++) {
+            String maybeDob = dateOfBirth.substring(i, i + 4);
+            try {
+                int year = Integer.parseInt(maybeDob);
+                fourConsecutiveNumbers.add(year);
+            } catch (NumberFormatException e) {
+                log.error("Unable to parse this string into int: " + maybeDob);
+            }
+        }
 
+        int dobYear = fourConsecutiveNumbers.stream().filter(getReasonableDob()).toList().getFirst();
 
+        return currentYear - dobYear;
+    }
 
-
+    private static Predicate<Integer> getReasonableDob() {
+        return year -> year > 1900 && year < 2024;
     }
 
 }
