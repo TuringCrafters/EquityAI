0f7d7e98d098cf10e47ee176874e22fe13614d46
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-01-25 10:53:28 +0100

    Create age converter function

---
+++ server/src/main/java/ai/equity/salt/openai/file/datapoint/AgeConverter.java
@@ -0,0 +1,20 @@
+package ai.equity.salt.openai.file.datapoint;
+
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+
+@RequiredArgsConstructor
+public class AgeConverter {
+
+    public static int calculateAge(String dateOfBirth){
+
+        int currentYear = LocalDate.now().getYear();
+
+
+
+
+
+    }
+
+}
