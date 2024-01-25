c5c25320d9691c3d769a32e0871fb45acf3f3012
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-01-25 09:04:41 +0100

    edit type to accept null values

--- client/types/TransformedExperienceDetails.ts
+++ client/types/TransformedExperienceDetails.ts
@@ -1,6 +1,6 @@
 export type TransformedExperienceDetails = {
     yearsOfExperience: number;
     salary_average: number;
-    salary_above_average: number;
-    salary_below_average: number;
+    salary_above_average: number | null;
+    salary_below_average: number | null;
   };
