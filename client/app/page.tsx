3223f3679af66ff90d90a98eccc0f2d763692f8b
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-26 16:17:35 +0100

    remove unused code

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,13 +1,8 @@
 "use client";
-
-import UploadFile from "@/components/Upload";
-import SampleFileForUpload from "@/components/SampleFile";
 import HomePage from "@/components/HomePage";
 
 export default function Home() {
   return <div>
-  {/* <UploadFile/>
-  <SampleFileForUpload/> */}
   <HomePage />
 </div>
 }
