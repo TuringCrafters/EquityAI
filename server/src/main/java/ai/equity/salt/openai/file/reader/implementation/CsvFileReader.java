d57fc1ea1193c7e17539d106129c081f73da8f7c
Ibrahim <Ibrahim@ebs.net.pk>
2024-01-26 09:47:27 +0100

    update method to return List of JobDataSet

--- server/src/main/java/ai/equity/salt/openai/file/reader/implementation/CsvFileReader.java
+++ server/src/main/java/ai/equity/salt/openai/file/reader/implementation/CsvFileReader.java
@@ -1,5 +1,6 @@
 package ai.equity.salt.openai.file.reader.implementation;
 
+import ai.equity.salt.openai.controller.dto.JobDataSet;
 import ai.equity.salt.openai.file.reader.FileReader;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
@@ -14,19 +15,27 @@ import java.util.List;
 public class CsvFileReader implements FileReader {
 
     @Override
-    public List<List<String>> readFile(MultipartFile file) {
+    public List<JobDataSet> readFile(MultipartFile file) {
 
-        List<List<String>> jobDataList = new ArrayList<>();
+        List<JobDataSet> jobDataList = new ArrayList<>();
 
         try (CSVReader csvReader = new CSVReader((new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8)))) {
             csvReader.readNext();
 
             String[] nextRecord;
             while ((nextRecord = csvReader.readNext()) != null) {
-                if (nextRecord.length < 5) {
+                if (nextRecord.length < 6) {
                     continue;
                 }
-                jobDataList.add(List.of(nextRecord));
+                JobDataSet jobData = new JobDataSet();
+                jobData.setPosition(nextRecord[0]);
+                jobData.setSalary(Double.parseDouble(nextRecord[1]));
+                jobData.setExperience(Integer.parseInt(nextRecord[2]));
+                jobData.setAge(Integer.parseInt(nextRecord[3]));
+                jobData.setGeographicLocation(nextRecord[4]);
+                jobData.setGender(nextRecord[5]);
+
+                jobDataList.add(jobData);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
