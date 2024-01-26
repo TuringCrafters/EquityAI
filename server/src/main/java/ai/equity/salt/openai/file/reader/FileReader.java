da1fa11f8b36fda1b00d4e0fd1e7b68540ceb494
Ibrahim <Ibrahim@ebs.net.pk>
2024-01-26 11:31:34 +0100

    Refactor FileReader interface to accept InputStream instead of MultipartFile

--- server/src/main/java/ai/equity/salt/openai/file/reader/FileReader.java
+++ server/src/main/java/ai/equity/salt/openai/file/reader/FileReader.java
@@ -1,10 +1,10 @@
 package ai.equity.salt.openai.file.reader;
 
 import ai.equity.salt.openai.controller.dto.JobDataSet;
-import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
 import java.util.List;
 @FunctionalInterface
 public interface FileReader {
-    List<JobDataSet> readFile(MultipartFile file);
+    List<JobDataSet> readFile(InputStream fileInputStream);
 }
