68c3e9376b02e1a4dab2182b809bb92fdc61c9d6
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-26 16:34:49 +0100

    render navbar in home page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 import HomePage from "@/components/HomePage";
+import NavBar from "@/components/Navbar";
 
 export default function Home() {
   return <div>
+  <NavBar />
   <HomePage />
 </div>
 }
