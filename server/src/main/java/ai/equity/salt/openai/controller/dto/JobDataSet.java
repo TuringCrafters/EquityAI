5171e703658cb5d8075c9337ecb92e38e099637b
Ibrahim <Ibrahim@ebs.net.pk>
2024-01-26 09:50:25 +0100

    add constructor that uses list

--- server/src/main/java/ai/equity/salt/openai/controller/dto/JobDataSet.java
+++ server/src/main/java/ai/equity/salt/openai/controller/dto/JobDataSet.java
@@ -1,8 +1,12 @@
 package ai.equity.salt.openai.controller.dto;
 
+import ai.equity.salt.openai.file.datapoint.DateConverter;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.util.List;
 
 @Setter
 @Getter
@@ -17,4 +21,14 @@ public class JobDataSet {
     private String currency;
     private int experience;
     private String geographicLocation;
+
+    public JobDataSet(List<String> rowData) {
+        this.gender = rowData.get(1);
+        this.age = DateConverter.calculateYears(rowData.get(2));
+        this.experience = DateConverter.calculateYears(rowData.get(3));
+        this.position = rowData.get(4);
+        this.salary = Double.parseDouble(rowData.get(5));
+        this.currency = rowData.get(6);
+        this.geographicLocation = rowData.get(9);
+    }
 }
