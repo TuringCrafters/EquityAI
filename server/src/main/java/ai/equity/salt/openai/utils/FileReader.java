195794508c5c474b9dca43824d2d5afd07de8a37
AnneMariel95 <a.mariel.recio@gmail.com>
2024-01-26 09:22:20 +0100

    create methods to convert excel to csv and read data from converted file

--- server/src/main/java/ai/equity/salt/openai/utils/FileReader.java
+++ server/src/main/java/ai/equity/salt/openai/utils/FileReader.java
@@ -36,4 +36,55 @@ public class FileReader {
         }
         return jobDataList;
     }
+
+    public static List<JobDataSet> readCSVFromExcel(InputStream inputStream) throws IOException, CsvValidationException {
+        List<JobDataSet> jobDataList = new ArrayList<>();
+
+        try (CSVReader csvReader = new CSVReader((new InputStreamReader(inputStream, StandardCharsets.UTF_8)))) {
+            csvReader.readNext();
+
+            String[] nextRecord;
+            while ((nextRecord = csvReader.readNext()) != null) {
+                if (nextRecord.length < 13) {
+                    continue;
+                }
+                JobDataSet jobData = new JobDataSet();
+                jobData.setId(nextRecord[0]);
+                jobData.setPosition(nextRecord[4]);
+                jobData.setSalary(Double.parseDouble(nextRecord[5]));
+                jobData.setExperience((int)Double.parseDouble(nextRecord[6]));
+                jobData.setAge((int)Double.parseDouble(nextRecord[6] + 10));
+                jobData.setGeographicLocation(nextRecord[9]);
+
+                jobDataList.add(jobData);
+            }
+        }
+        return jobDataList;
+    }
+
+    public static InputStream convertXlsxToCsv(InputStream inputStream) throws IOException {
+        Workbook workbook = new XSSFWorkbook(inputStream);
+        Sheet sheet = workbook.getSheetAt(0);
+
+        ByteArrayOutputStream csvOutputStream = new ByteArrayOutputStream();
+        PrintStream csvPrintStream = new PrintStream(csvOutputStream);
+
+        for (Row row : sheet) {
+            for (Cell cell : row) {
+                String cellValue = "";
+                if (cell.getCellType() == CellType.STRING) {
+                    cellValue = cell.getStringCellValue();
+                } else if (cell.getCellType() == CellType.NUMERIC) {
+                    cellValue = String.valueOf(cell.getNumericCellValue());
+                } else if (cell.getCellType() == CellType.BOOLEAN) {
+                    cellValue = String.valueOf(cell.getBooleanCellValue());
+                }
+                csvPrintStream.print(cellValue);
+                csvPrintStream.print(",");
+            }
+            csvPrintStream.println();
+        }
+        csvPrintStream.flush();
+        return new ByteArrayInputStream(csvOutputStream.toByteArray());
+    }
 }
