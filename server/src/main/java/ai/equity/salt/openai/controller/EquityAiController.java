5869b30e5954a21ee465bd6cf93986cfda4a89f6
Ibrahim <Ibrahim@ebs.net.pk>
2024-01-26 11:30:45 +0100

    Refactor file handling methods in EquityAiController

--- server/src/main/java/ai/equity/salt/openai/controller/EquityAiController.java
+++ server/src/main/java/ai/equity/salt/openai/controller/EquityAiController.java
@@ -22,13 +22,13 @@ public class EquityAiController {
 
     @PostMapping("file/analyze")
     @ResponseStatus(CREATED)
-    public EquityAiResponse sendFile(@RequestParam MultipartFile file) throws IOException, CsvValidationException {
+    public EquityAiResponse sendFile(@RequestParam MultipartFile file) throws IOException {
         return service.analyzeFile(file);
     }
 
     @PostMapping("file/any")
     @ResponseStatus(CREATED)
-    public List<JobDataSet> sendAnyFile(@RequestParam MultipartFile file) {
+    public List<JobDataSet> sendAnyFile(@RequestParam MultipartFile file) throws IOException {
         return service.readAnyFile(file);
     }
 }
