249b572d4f63d4c17d20bdd8e3b2b9ad4c9739fa
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-26 16:52:14 +0100

    remove unused import

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "../services/provider";
 import { Toaster } from "@/components/ui/toaster";
-import NavBar from "@/components/Navbar";
 /* import Footer from "@/components/Footer"; */
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,7 +19,6 @@ export default function RootLayout({
     <Providers>
       <html lang="en" className="h-full">
         <body className={inter.className}>
-          {/* <NavBar /> */}
           {children}
           <Toaster />
         {/*   <Footer /> */}
