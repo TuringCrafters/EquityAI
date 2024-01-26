61ecb49d3e7b673403bf48c004e4961c2f7904fb
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-26 15:35:53 +0100

    change navbar styling

--- client/components/Navbar/index.tsx
+++ client/components/Navbar/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 export default function NavBar() {
   return (
-    <nav className="z-20 top-0 left-0 right-0 h-12">
+    <nav className="fixed z-20 top-0 left-0 right-0 h-12 bg-white">
       <h1 className="text-2xl font-semibold ml-20  mt-2 tracking-tighter text-slate-950">
         equityAi
       </h1>
