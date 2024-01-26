29ddae52b417eb7986162db241f098422eac602c
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-01-26 12:07:17 +0100

    Create function to split insights text into two blocks of text

---
+++ client/services/splitTextIntoTwoParts.ts
@@ -0,0 +1,7 @@
+export const splitTextIntoTwoParts = (text: String ) => {
+    const splitText = text?.split('.') || [];
+    const midpoint = Math.ceil(splitText.length / 2);
+    const firstHalf = splitText.slice(0, midpoint).join('.');
+    const secondHalf = splitText.slice(midpoint).join('.');
+    return { firstHalf, secondHalf };
+  };
