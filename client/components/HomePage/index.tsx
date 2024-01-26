6e850af2f8d3148e0ab94fbe71e04c0440aadaab
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-26 11:54:16 +0100

    render arrow icon in home page with onClick functionality

--- client/components/HomePage/index.tsx
+++ client/components/HomePage/index.tsx
@@ -1,3 +1,4 @@
+import ArrowRightIcon from '@/public/icon/arrowRightIcon';
 import InfoIcon from '@/public/icon/infoIcon';
 import React, { useState } from 'react';
 
@@ -7,13 +8,12 @@ const HomePage = () => {
     return (
         <div className="flex flex-col md:flex-row h-screen overflow-hidden">
             <div className={`bg-white fixed z-10 h-full ${showLeftOnMobile ? 'slide-in' : 'slide-out'} md:relative md:w-1/4`}>
-                <button
-                    className="mt-40 ml-4 md:hidden"
-                    onClick={() => setShowLeftOnMobile(false)}
-                >
-                    Show Right
-                </button>
-                <p className="text-center text-white">Left Content</p>
+                <div className="flex justify-end">
+                    <ArrowRightIcon
+                        className="w-6 h-6 mt-16 mr-4 md:hidden"
+                        onClick={() => setShowLeftOnMobile(false)} />
+                </div>
+                <p className="text-center text-black">Left Content</p>
             </div>
             <div className={`bg-cover bg-center bg-no-repeat h-screen ${!showLeftOnMobile || 'md:block'} md:w-3/4 h-full`}
                 style={{ backgroundImage: 'url(https://www.sysarb.se/wp-content/uploads/2023/05/Gradient_final_1-scaled.jpg)' }}
