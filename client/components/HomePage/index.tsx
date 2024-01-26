c2136c6e0456ce318a506fb9fc211f7dd09d75e1
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-26 11:17:24 +0100

    add background to right component in home page

--- client/components/HomePage/index.tsx
+++ client/components/HomePage/index.tsx
@@ -5,7 +5,7 @@ const HomePage = () => {
 
   return (
     <div className="flex flex-col md:flex-row h-screen overflow-hidden">
-      <div className={`bg-blue-500 fixed z-10 h-full ${showLeftOnMobile ? 'slide-in' : 'slide-out'} md:relative md:w-1/4`}>
+      <div className={`bg-white fixed z-10 h-full ${showLeftOnMobile ? 'slide-in' : 'slide-out'} md:relative md:w-1/4`}>
         <button
           className="mt-40 ml-4 md:hidden"
           onClick={() => setShowLeftOnMobile(false)}
@@ -14,7 +14,9 @@ const HomePage = () => {
         </button>
         <p className="text-center text-white">Left Content</p>
       </div>
-      <div className={`bg-green-500 ${!showLeftOnMobile || 'md:block'} md:w-3/4 h-full`}>
+      <div className={`bg-cover bg-center bg-no-repeat h-screen ${!showLeftOnMobile || 'md:block'} md:w-3/4 h-full`}
+      style={{ backgroundImage: 'url(https://www.sysarb.se/wp-content/uploads/2023/05/Gradient_final_1-scaled.jpg)'}}
+      >
         <button
           className="mt-40 ml-4 md:hidden"
           onClick={() => setShowLeftOnMobile(true)}
