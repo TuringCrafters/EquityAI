d5093250d84f3dd3fa142e8e6c269a53c2ac3afc
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-01-29 11:40:42 +0100

    Refactor pie gap chart.

--- client/components/PayGapChart/idex.tsx
+++ client/components/PayGapChart/idex.tsx
@@ -12,14 +12,14 @@ export const PayGapPieChart = () => {
   }
 
   const genderPayGap: number = parseFloat(data.gender_pay_gap);
-  let noPayGap: number = 0;
   let womenPayGap: number = 0;
 
-  if (genderPayGap === 0) {
-    noPayGap += genderPayGap;
-  } else {
+  if (genderPayGap !== 0) {
     womenPayGap = 100 - 100 * genderPayGap;
   }
+
+  console.log(genderPayGap);
+  console.log(womenPayGap);
 
   const pieChartData = [
     { name: "Woman", value: womenPayGap },
@@ -29,7 +29,7 @@ export const PayGapPieChart = () => {
     <div className="flex items-center justify-around mx-40 my-8 py-5 bg-white rounded-lg shadow-md">
       <div>
         <p className="text-lg text-neutral-700 font-bold pl-10">
-          {noPayGap === 0 ? (
+          {genderPayGap === 0 ? (
             <>
               Congratulations, there is no pay gap <br /> in your company
               between different genders.
@@ -72,20 +72,16 @@ export const PayGapPieChart = () => {
               d="M160 0a48 48 0 1 1 0 96 48 48 0 1 1 0-96zM88 384H70.2c-10.9 0-18.6-10.7-15.2-21.1L93.3 248.1 59.4 304.5c-9.1 15.1-28.8 20-43.9 10.9s-20-28.8-10.9-43.9l53.6-89.2c20.3-33.7 56.7-54.3 96-54.3h11.6c39.3 0 75.7 20.6 96 54.3l53.6 89.2c9.1 15.1 4.2 34.8-10.9 43.9s-34.8 4.2-43.9-10.9l-33.9-56.3L265 362.9c3.5 10.4-4.3 21.1-15.2 21.1H232v96c0 17.7-14.3 32-32 32s-32-14.3-32-32V384H152v96c0 17.7-14.3 32-32 32s-32-14.3-32-32V384z"
             />
           </svg>
-          {noPayGap === 0 ? (
-            
-              <div className="flex flex-col pt-4">
-                <p className="text-sm text-neutral-500">Women receive</p>
-                <h4 className=" font-bold text-4xl">100%</h4>
-              </div>
-            
+          {genderPayGap === 0 ? (
+            <div className="flex flex-col pt-4">
+              <p className="text-sm text-neutral-500">Women receive</p>
+              <h4 className=" font-bold text-4xl">100%</h4>
+            </div>
           ) : (
-            
-              <div className="flex flex-col pt-4">
-                <p className="text-sm text-neutral-500">Women only receive</p>
-                <h4 className=" font-bold text-4xl">{womenPayGap}%</h4>{" "}
-              </div>
-            
+            <div className="flex flex-col pt-4">
+              <p className="text-sm text-neutral-500">Women only receive</p>
+              <h4 className=" font-bold text-4xl">{womenPayGap}%</h4>{" "}
+            </div>
           )}
         </div>
       </div>
