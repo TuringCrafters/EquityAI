9a6f8ca865d0e15612acad9c219c139127ecc3d1
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-30 14:17:55 +0100

    add router to push to developers page from home page nav bar

--- client/components/Navbar/index.tsx
+++ client/components/Navbar/index.tsx
@@ -1,11 +1,15 @@
 "use client";
+import { useRouter } from "next/navigation";
+
 export default function NavBar() {
+  const router = useRouter();
   return (
     <nav className="fixed flex justify-between z-20 top-0 left-0 right-0 h-12 bg-neutral-100 shadow md:shadow-lg">
       <h1 className="text-2xl font-semibold ml-20  mt-2 tracking-tighter text-slate-950">
         equityAi
       </h1>
-      <button className="mr-8 hover:text-violet-800">About developers</button>
+      <button className="mr-8 hover:text-violet-800" 
+      onClick={() => router.push("/developers")}>About developers</button>
     </nav>
   );
 }
