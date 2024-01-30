e6a97ad8c317dcf67378984b7fdbd27465765907
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-30 12:03:57 +0100

    add developer info

---
+++ client/services/devInfo.ts
@@ -0,0 +1,31 @@
+export const developers = [
+    {
+        name: 'Ibrahim Iqbal', role: 'Full-stack Developer',
+        avatar: "Ibrahim.png",
+        info: "Dedicated Java developer with a passion for coding and backend development. Thrives on challenges and innovation. Seeking to bring expertise and dedication to your company.",
+        linkedin: "https://www.linkedin.com/in/ibrahim-iqbal-dib59/",
+        github: "https://github.com/dIB59"
+    },
+    {
+        name: 'Anastasia Kurayshevich', role: 'Full-stack Developer',
+        avatar: "Anastasia.png",
+        info: "Driven Java and TypeScript developer with a passion for problem-solving and a strong commitment to continuous learning.",
+        linkedin: "https://www.linkedin.com/in/anastasia-kurayshevich/",
+        github: "https://github.com/AnastasiaKurayshevich"
+    },
+    {
+        name: 'Marisa Pinheiro', role: 'Full-stack Developer',
+        avatar: "Marisa.png",
+        info: "Fullstack Developer with an ambition to develop sustainable solutions, creative technologies and projects filled with beautiful details.",
+        linkedin: "https://www.linkedin.com/in/marisa-pinheiro-833a12113",
+        github: "https://github.com/Marisa-Pinheiro"
+    },
+    {
+        name: 'Anne Mariel Catapang', role: 'Full-stack Developer',
+        avatar: "Mariel.png",
+        info: "A business strategist turned Java developer dedicated to empowering businesses in achieving their optimal IT environment",
+        linkedin: "https://www.linkedin.com/in/anne-mariel/",
+        github: "https://github.com/AnneMariel95"
+    }
+
+];
