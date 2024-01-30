55c865a3f09ea2bfc022c01fd192ed6fe6129903
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-30 11:06:59 +0100

    style footer size

--- client/components/Footer/index.tsx
+++ client/components/Footer/index.tsx
@@ -4,9 +4,9 @@ import GitHubIcon from "@/public/icon/gitHubIcon";
 export default function Footer() {
   return (
     <footer className="bottom-0 w-full default-gradient">
-      <div className="flex flex-row justify-between py-12 px-20 items-center">
+      <div className="flex flex-row justify-between pt-24 pb-14 px-24 items-center">
         <div>
-          <div>&copy; EquityAi, 2024</div>
+          <div>&copy;EquityAi, 2024</div>
           <button>About developers</button>
         </div>
         <div className="">
