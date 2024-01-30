4b2b0bf5bf17b9761c3e82b61601c4e3d0c7ca18
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-30 15:32:27 +0100

    refactore code to remove link from inside the button

--- client/components/SampleFile/index.tsx
+++ client/components/SampleFile/index.tsx
@@ -5,7 +5,9 @@ const SampleFileForUpload = () => {
     const link = document.createElement("a");
     link.href = `/samples/${fileName}`;
     link.download = fileName;
+    document.body.appendChild(link); 
     link.click();
+    document.body.removeChild(link);
   };
 
   return (
@@ -28,9 +30,7 @@ const SampleFileForUpload = () => {
             d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
           />
         </svg>
-        <Link href={"/samples/Dataset.xlsx"} target="_blank" download>
           Dataset.xlsx
-        </Link>
       </button>
     </div>
   );
