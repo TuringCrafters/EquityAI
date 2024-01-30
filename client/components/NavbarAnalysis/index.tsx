3d6ce8aade41e43b20025bbc7e0354c3e250c91c
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-30 14:34:47 +0100

    add router push to nav bar in analysis page

--- client/components/NavbarAnalysis/index.tsx
+++ client/components/NavbarAnalysis/index.tsx
@@ -1,8 +1,12 @@
 "use client";
+import { useRouter } from "next/navigation";
+
 export default function NavBarAnalysis() {
+  const router = useRouter();
   return (
     <nav className="fixed z-20 top-0 left-0 right-0 h-12 ">
-      <h1 className="text-2xl font-semibold ml-20  mt-2 tracking-tighter text-slate-950">
+      <h1 className="text-2xl font-semibold ml-20  mt-2 tracking-tighter text-slate-950 hover:cursor-pointer"
+      onClick={() => router.push('/')}>
         equityAi
       </h1>
     </nav>
