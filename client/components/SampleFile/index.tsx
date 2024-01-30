f4d16d3165f7645b1e45170b4677ac9ba56e1db8
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-30 15:24:32 +0100

    refactor the download component to only render one file for downloading

--- client/components/SampleFile/index.tsx
+++ client/components/SampleFile/index.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link";
 
-const samples = ["Dataset.xlsx"];
-
 const SampleFileForUpload = () => {
   const handleDownload = (fileName: string) => {
     const link = document.createElement("a");
@@ -12,36 +10,28 @@ const SampleFileForUpload = () => {
 
   return (
     <div className="flex flex-col items-center justify-center text-black ">
-      <ul className="flex flex-col gap-1 list-none">
-        {samples.map((fileName) => {
-          return (
-            <li key={fileName} className="list-none flex items-center">
-              <button
-                className="mt-2 flex items-center bg-purple-600 hover:bg-purple-700 text-white rounded-full px-3 py-2"
-                onClick={() => handleDownload(fileName)}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6 mr-2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
-                  />
-                </svg>
-                <Link href={`/samples/${fileName}`} target="_blank" download>
-                  {fileName}
-                </Link>
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+      <button
+        className="mt-2 flex items-center bg-purple-600 hover:bg-purple-700 text-white rounded-full px-3 py-2"
+        onClick={() => handleDownload("Dataset.xlsx")}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6 mr-2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+          />
+        </svg>
+        <Link href={"/samples/Dataset.xlsx"} target="_blank" download>
+          Dataset.xlsx
+        </Link>
+      </button>
     </div>
   );
 };
