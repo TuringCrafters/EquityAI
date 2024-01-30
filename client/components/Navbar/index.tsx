b6446dfd8705c53e00b382ba5f13cda6b2dc4c0d
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-30 14:37:58 +0100

    refactor navbar to handle optional fields

--- client/components/Navbar/index.tsx
+++ client/components/Navbar/index.tsx
@@ -3,18 +3,20 @@ import { NavBarProps } from "@/types/NavBarProps";
 import { useRouter } from "next/navigation";
 
 const NavBar: React.FC<NavBarProps> = ({
-  link, 
+  link,
   buttonContent
 }) => {
   const router = useRouter();
   return (
     <nav className="fixed flex justify-between z-20 top-0 left-0 right-0 h-12 bg-neutral-100 shadow md:shadow-lg">
       <h1 className="text-2xl font-semibold ml-20  mt-2 tracking-tighter text-slate-950 hover:cursor-pointer"
-      onClick={() => router.push('/')}>
+        onClick={() => router.push('/')}>
         equityAi
       </h1>
-      <button className="mr-8 hover:text-violet-800" 
-      onClick={() => router.push(`/${link}`)}>{buttonContent}</button>
+      {link && buttonContent &&
+        <button className="mr-8 hover:text-violet-800"
+          onClick={() => router.push(`/${link}`)}>{buttonContent}</button>
+      }
     </nav>
   );
 }
