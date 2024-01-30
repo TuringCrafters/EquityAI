f303efb672b9f9b6238d0a953c5d62a535ab5ca2
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-30 11:10:45 +0100

    add router to push to about developers page

--- client/components/Footer/index.tsx
+++ client/components/Footer/index.tsx
@@ -1,13 +1,15 @@
 "use client";
 import GitHubIcon from "@/public/icon/gitHubIcon";
+import { useRouter } from "next/navigation";
 
 export default function Footer() {
+  const router = useRouter();
   return (
     <footer className="bottom-0 w-full default-gradient">
       <div className="flex flex-row justify-between pt-24 pb-14 px-24 items-center">
         <div>
           <div>&copy;EquityAi, 2024</div>
-          <button>About developers</button>
+          <button onClick={() => router.push("/developers")}>About developers</button>
         </div>
         <div className="">
           <h5>Turing Crafters</h5>
