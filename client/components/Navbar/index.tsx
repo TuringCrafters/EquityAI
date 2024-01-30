8c1b09fdf7356f715e9dbf93ac19f371f3956f21
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-30 14:15:02 +0100

    add about developers to home page nav bar

--- client/components/Navbar/index.tsx
+++ client/components/Navbar/index.tsx
@@ -1,10 +1,11 @@
 "use client";
 export default function NavBar() {
   return (
-    <nav className="fixed z-20 top-0 left-0 right-0 h-12 bg-neutral-100 shadow md:shadow-lg">
+    <nav className="fixed flex justify-between z-20 top-0 left-0 right-0 h-12 bg-neutral-100 shadow md:shadow-lg">
       <h1 className="text-2xl font-semibold ml-20  mt-2 tracking-tighter text-slate-950">
         equityAi
       </h1>
+      <button className="mr-8 hover:text-violet-800">About developers</button>
     </nav>
   );
 }
