6d73ac7fc8d23a33fdfebf485f9b82e320d20227
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-30 15:35:40 +0100

    refactor code to use download icon component

--- client/components/SampleFile/index.tsx
+++ client/components/SampleFile/index.tsx
@@ -1,3 +1,5 @@
+import DownloadIcon from "@/public/icon/downloadIcon";
+import { Download } from "lucide-react";
 import Link from "next/link";
 
 const SampleFileForUpload = () => {
@@ -5,7 +7,7 @@ const SampleFileForUpload = () => {
     const link = document.createElement("a");
     link.href = `/samples/${fileName}`;
     link.download = fileName;
-    document.body.appendChild(link); 
+    document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   };
@@ -16,21 +18,8 @@ const SampleFileForUpload = () => {
         className="mt-2 flex items-center bg-purple-600 hover:bg-purple-700 text-white rounded-full px-3 py-2"
         onClick={() => handleDownload("Dataset.xlsx")}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6 mr-2"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
-          />
-        </svg>
-          Dataset.xlsx
+        <DownloadIcon />
+        Dataset.xlsx
       </button>
     </div>
   );
