e512692874cf2f50d90058227a55566cdb3c952e
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-30 13:39:21 +0100

    style developers page

--- client/app/developers/page.tsx
+++ client/app/developers/page.tsx
@@ -5,13 +5,15 @@ import { LinkedinIcon } from 'lucide-react';
 import React from 'react'
 
 const DeveloperCard: React.FC<DeveloperCardProps> = ({ name, role, avatar, info, linkedin, github }) => (
-    <div className="card bg-white shadow-md rounded-lg overflow-hidden">
+    <div className="card bg-white shadow-lg rounded-2xl overflow-hidden h-[450px] flex flex-col">
         <img src={`/devAvatars/${avatar}`} alt={`${name} avatar`} className="w-40 h-40 mx-auto mt-4" />
-        <div className="p-4">
-            <h6 className="text-center text-lg font-semibold">{name}</h6>
-            <p className="text-center text-sm text-gray-600 py-2">{role}</p>
-            <p className="text-center text-sm text-gray-600">{info}</p>
-            <div className="flex justify-center mt-4">
+        <div className="flex-1 p-4 flex flex-col justify-between overflow-auto">
+            <div>
+                <h6 className="text-center text-lg font-semibold">{name}</h6>
+                <p className="text-center text-sm text-gray-600 py-2">{role}</p>
+                <p className="text-center text-sm text-gray-600">{info}</p>
+            </div>
+            <div className="flex justify-center items-center my-4 ">
                 {linkedin && (
                     <a href={linkedin} target="_blank" rel="noopener noreferrer" className="text-blue-500 mx-2">
                         <LinkedinIcon />
@@ -29,20 +31,20 @@ const DeveloperCard: React.FC<DeveloperCardProps> = ({ name, role, avatar, info,
 
 const Developers = () => {
     return (
-        <div className="container mx-auto my-8 px-4">
-                <h3 className="text-center text-3xl font-bold mb-4">Developer Page</h3>
-                <div className="bg-white shadow-md p-8 mb-6">
-                    <p className="text-center">
-                        Welcome to the Developer Page! Here you can find information and resources for developers working on our project.
-                    </p>
-                </div>
-                <h4 className="text-center text-2xl font-bold mb-4">Meet the Team</h4>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
-                    {developers.map((developer, index) => (
-                        <DeveloperCard key={index} {...developer} />
-                    ))}
-                </div>
+        <div className="container mx-auto my-8 px-16">
+            <h3 className="text-center text-3xl font-bold mb-4">Developers Page</h3>
+            <div className="bg-white shadow-md rounded-2xl p-8 mb-6">
+                <p className="text-center">
+                    Welcome! Here you can find information and resources for developers working on EquityAI project.
+                </p>
             </div>
+            <h4 className="text-center text-2xl font-bold my-8">Meet the Team</h4>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                {developers.map((developer, index) => (
+                    <DeveloperCard key={index} {...developer} />
+                ))}
+            </div>
+        </div>
     )
 }
 
