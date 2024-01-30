682a1118c8f2f631fed948b9512f216f726bb800
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-30 16:39:45 +0100

    apply classes

--- client/containers/analysis-page/overal-analysis-section/index.tsx
+++ client/containers/analysis-page/overal-analysis-section/index.tsx
@@ -28,13 +28,13 @@ export const OveralAnalysis = () => {
   );
 
   return (
-    <section className="flex flex-row justify-evenly mx-20 mt-10 print">
-      <article className="w-1/2 border-r-2 border-neutral-200">
+    <section className="flex flex-row justify-evenly mx-20 mt-10 print print-overall">
+      <article className="w-1/2 border-r-2 border-neutral-200 print-overall__chart">
         <div className="px-20">
           <h2 className="text-slate-950 text-2xl font-bold">
             Your company average worker
           </h2>
-          <div className="flex flex-row justify-around my-20">
+          <div className="flex flex-row justify-around my-20 print-overall__boxes">
             <div className="bg-neutral-200 rounded-lg h-24 w-24 text-center py-4">
               <p className="font-medium">Age</p>
               <p className="text-xl font-bold">
@@ -99,7 +99,7 @@ export const OveralAnalysis = () => {
           </div>
         </div>
       </article>
-      <article className="w-1/2">
+      <article className="w-1/2 print-overall__chart">
         <div className="px-20">
           <h2 className="text-slate-950 text-2xl font-bold mb-4">
             Your biggest earners
