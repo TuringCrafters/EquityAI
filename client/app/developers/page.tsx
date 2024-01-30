a4a813eadc773063ca9d5aff9f4bb1c95395970d
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-30 12:04:40 +0100

    render developer information

--- client/app/developers/page.tsx
+++ client/app/developers/page.tsx
@@ -1,4 +1,5 @@
 import GitHubIcon from '@/public/icon/gitHubIcon';
+import { developers } from '@/services/devInfo';
 import { DeveloperCardProps } from '@/types/DeveloperCardProps';
 import { LinkedinIcon } from 'lucide-react';
 import React from 'react'
@@ -28,7 +29,20 @@ const DeveloperCard: React.FC<DeveloperCardProps> = ({ name, role, avatar, info,
 
 const Developers = () => {
     return (
-        <div>Developers</div>
+        <div className="container mx-auto my-8 px-4">
+                <h3 className="text-center text-3xl font-bold mb-4">Developer Page</h3>
+                <div className="bg-white shadow-md p-8 mb-6">
+                    <p className="text-center">
+                        Welcome to the Developer Page! Here you can find information and resources for developers working on our project.
+                    </p>
+                </div>
+                <h4 className="text-center text-2xl font-bold mb-4">Meet the Team</h4>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                    {developers.map((developer, index) => (
+                        <DeveloperCard key={index} {...developer} />
+                    ))}
+                </div>
+            </div>
     )
 }
 
