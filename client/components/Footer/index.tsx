0f6454a79dfc50ac275f7b9e20a8909cc0183d65
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-30 11:03:00 +0100

    delete unused code

--- client/components/Footer/index.tsx
+++ client/components/Footer/index.tsx
@@ -4,15 +4,10 @@ import GitHubIcon from "@/public/icon/gitHubIcon";
 export default function Footer() {
   return (
     <footer className="bottom-0 w-full default-gradient">
-      {/* <div className=" flex flex-row align-middle items-center">
-          <h1 className="text-2xl font-semibold ml-20 my-10 tracking-tighter">
-            company.
-          </h1>
-      </div> */}
       <div className="flex flex-row justify-between py-12 px-20 items-center">
         <div>
-        <div>&copy; EquityAi, 2024</div>
-        <button>About developers</button>
+          <div>&copy; EquityAi, 2024</div>
+          <button>About developers</button>
         </div>
         <div className="">
           <h5>Turing Crafters</h5>
