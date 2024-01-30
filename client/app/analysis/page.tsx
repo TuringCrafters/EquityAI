3b05707dc6c77afed8b6f3ee5365b6817380dbb4
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-01-30 16:13:21 +0100

    Refactor analysis page

--- client/app/analysis/page.tsx
+++ client/app/analysis/page.tsx
@@ -3,14 +3,12 @@ import React, { useRef } from "react";
 import { useReactToPrint } from "react-to-print";
 import { Button } from "../../components/ui/button";
 import "../../styles/AnalysisPagePdfStyles.css";
-import { AnalysisHeader } from '../../containers/analysis-page/header-section/index';
+import { AnalysisHeader } from "../../containers/analysis-page/header-section/index";
 import { OveralAnalysis } from "@/containers/analysis-page/overal-analysis-section";
 import { MostCommonPosition } from "@/containers/analysis-page/most-common-position-section";
 import { Insights } from "@/containers/analysis-page/insights-section/indext";
 import { Recommendations } from "@/containers/analysis-page/recomendation-section";
 import NavBarAnalysis from "@/components/NavbarAnalysis";
-
-
 
 const AnalysisPage = () => {
   const pageRef = useRef<HTMLElement | null>(null);
@@ -21,8 +19,8 @@ const AnalysisPage = () => {
 
   return (
     <main className="h-dvh" ref={pageRef}>
-      <NavBarAnalysis/>
-      <AnalysisHeader/>
+      <NavBarAnalysis />
+      <AnalysisHeader />
       <Button
         onClick={handlePDF}
         className="absolute top-4 right-6 rounded-full bg-blue-600 "
@@ -32,10 +30,10 @@ const AnalysisPage = () => {
       {/* <Button className="absolute top-4 right-36 rounded-full bg-blue-600 ">
         Share PDF
       </Button> */}
-      <OveralAnalysis/>
-      <Insights/>
-     <MostCommonPosition/>
-     <Recommendations/>
+      <OveralAnalysis />
+      <Insights />
+      <MostCommonPosition />
+      <Recommendations />
     </main>
   );
 };
