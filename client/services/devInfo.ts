fbf676d9620695460103f2cbacc27ab1c7deaadd
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-30 13:39:29 +0100

    edit info

--- client/services/devInfo.ts
+++ client/services/devInfo.ts
@@ -1,19 +1,5 @@
 export const developers = [
     {
-        name: 'Ibrahim Iqbal', role: 'Full-stack Developer',
-        avatar: "Ibrahim.png",
-        info: "Dedicated Java developer with a passion for coding and backend development. Thrives on challenges and innovation. Seeking to bring expertise and dedication to your company.",
-        linkedin: "https://www.linkedin.com/in/ibrahim-iqbal-dib59/",
-        github: "https://github.com/dIB59"
-    },
-    {
-        name: 'Anastasia Kurayshevich', role: 'Full-stack Developer',
-        avatar: "Anastasia.png",
-        info: "Driven Java and TypeScript developer with a passion for problem-solving and a strong commitment to continuous learning.",
-        linkedin: "https://www.linkedin.com/in/anastasia-kurayshevich/",
-        github: "https://github.com/AnastasiaKurayshevich"
-    },
-    {
         name: 'Marisa Pinheiro', role: 'Full-stack Developer',
         avatar: "Marisa.png",
         info: "Fullstack Developer with an ambition to develop sustainable solutions, creative technologies and projects filled with beautiful details.",
@@ -26,6 +12,19 @@ export const developers = [
         info: "A business strategist turned Java developer dedicated to empowering businesses in achieving their optimal IT environment",
         linkedin: "https://www.linkedin.com/in/anne-mariel/",
         github: "https://github.com/AnneMariel95"
+    },
+    {
+        name: 'Ibrahim Iqbal', role: 'Full-stack Developer',
+        avatar: "Ibrahim.png",
+        info: "Dedicated Java developer with a passion for coding and backend development. Thrives on challenges and innovation. Seeking to bring expertise and dedication to your company.",
+        linkedin: "https://www.linkedin.com/in/ibrahim-iqbal-dib59/",
+        github: "https://github.com/dIB59"
+    },
+    {
+        name: 'Anastasia Kurayshevich', role: 'Full-stack Developer',
+        avatar: "Anastasia.png",
+        info: "Driven Java and TypeScript developer with a passion for problem-solving and a strong commitment to continuous learning.",
+        linkedin: "https://www.linkedin.com/in/anastasia-kurayshevich/",
+        github: "https://github.com/AnastasiaKurayshevich"
     }
-
 ];
