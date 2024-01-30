e7730449e77cb803a12bf54847010d4f8943047f
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-30 10:37:19 +0100

    add some new content into footer

--- client/components/Footer/index.tsx
+++ client/components/Footer/index.tsx
@@ -1,4 +1,5 @@
 "use client";
+import GitHubIcon from "@/public/icon/gitHubIcon";
 
 export default function Footer() {
   return (
@@ -7,6 +8,18 @@ export default function Footer() {
           <h1 className="text-2xl font-semibold ml-20 my-10 tracking-tighter">
             company.
           </h1>
+      </div> */}
+      <div className="flex flex-row justify-between py-12 px-20 border-t-2 border-neutral-200 items-center">
+        <div>
+        <div>&copy; EquityAi, 2024</div>
+        <button>About developers</button>
+        </div>
+        <div className="">
+          <h5>Turing Crafters</h5>
+          <div className="flex items-center justify-center space-x-4">
+            <GitHubIcon />
+          </div>
+        </div>
       </div>
     </footer>
   );
