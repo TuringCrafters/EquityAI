fad6469990fa2b8cc2575028b37ed2003db1b91f
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-31 13:35:03 +0100

    wrap the chart in the div for styling purpouses

--- client/containers/analysis-page/most-common-position-section/index.tsx
+++ client/containers/analysis-page/most-common-position-section/index.tsx
@@ -11,36 +11,38 @@ export const MostCommonPosition = () => {
       <h2 className="text-slate-950 text-2xl font-bold title-for-print mb-3">
         Your most common position
       </h2>
-    <h3 className=" text-neutral-500 text-xl font-semibold ">
-      {data?.job_title}
-    </h3>
-    <section className="flex flex-col items-center md:flex-row md:items-start align-middle h-3/6 justify-center my-10">
-      <div className="items-center h-full border-r-2 border-r-neutral-200 print-position__chart">
-        {data?.location_details && (
-          <>
-            <h3 className="ml-10 mt-5 font-semibold tracking-tighter print-position__chart__title">
-              Salary based on Location
-            </h3>
-            <BoxPlotChart
-              data={data.location_details}
-            />
-          </>
-        )}
-      </div>
-      <div className="items-center  max-h-100 print-position__chart">
-        {data?.experience_details && (
-          <>
-            <h3 className="ml-10 mt-5 mb-10 font-semibold tracking-tighter print-position__chart__title-two">
-              Salary based on Years of Experience
-            </h3>
-            <LineOfBestFitChart
-              key="linechart"
-              data={data.experience_details}
-            />
-          </>
-        )}
-      </div>
-    </section>     
-  </section>
+      <h3 className=" text-neutral-500 text-xl font-semibold ">
+        {data?.job_title}
+      </h3>
+      <section className="flex flex-col items-center md:flex-row md:items-start align-middle h-3/6 justify-center my-10">
+        <div className="items-center h-full border-r-2 border-r-neutral-200 print-position__chart">
+          {data?.location_details && (
+            <>
+              <h3 className="ml-10 mt-5 font-semibold tracking-tighter print-position__chart__title">
+                Salary based on Location
+              </h3>
+              <div className="print-position__chart__boxplot">
+                <BoxPlotChart
+                  data={data.location_details}
+                />
+              </div>
+            </>
+          )}
+        </div>
+        <div className="items-center  max-h-100 print-position__chart">
+          {data?.experience_details && (
+            <>
+              <h3 className="ml-10 mt-5 mb-10 font-semibold tracking-tighter print-position__chart__title-two">
+                Salary based on Years of Experience
+              </h3>
+              <LineOfBestFitChart
+                key="linechart"
+                data={data.experience_details}
+              />
+            </>
+          )}
+        </div>
+      </section>
+    </section>
   )
 };
