485e945920d38b3c6ed48ba7051fb2acb7c44403
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-31 14:49:15 +0100

    delete unused import

--- client/components/Upload/index.tsx
+++ client/components/Upload/index.tsx
@@ -1,5 +1,4 @@
 "use client";
-import axios from "axios";
 import React, { ChangeEvent, useContext, useEffect, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useToast } from "../ui/use-toast";
