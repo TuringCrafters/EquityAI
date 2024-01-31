7531247aa531ac7e6504409670e7427dec26a5a7
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-01-31 11:30:36 +0100

    Remove uncessary convertion of type for location details

--- client/services/dataConverter.ts
+++ client/services/dataConverter.ts
@@ -1,24 +1,7 @@
 import { ExperienceDetails } from "@/types/ExperienceDetails";
-import { LocationDetails } from "@/types/LocationDetails";
 import { TransformedExperienceDetails } from "@/types/TransformedExperienceDetails";
-import { TransformedLocationDetails } from "@/types/TransformedLocationDetails";
 import { DataPoint } from "regression";
 import { decimalConverter } from "./decimalConverter";
-
-export function transformLocationDetails(
-  originalDetails: LocationDetails
-): TransformedLocationDetails {
-  return {
-    location: originalDetails.data_value,
-    salary_average: decimalConverter(originalDetails.salary.average / 1000),
-    salary_above_average: decimalConverter(
-      originalDetails.salary.above_average / 1000
-    ),
-    salary_below_average: decimalConverter(
-      originalDetails.salary.below_average / 1000
-    ),
-  };
-}
 
 export function transformExperienceDetails(
   originalDetails: ExperienceDetails
