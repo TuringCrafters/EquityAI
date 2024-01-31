ece51ae13aa4fd03c2eec63da9c4d92e2a056830
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-31 15:12:27 +0100

    refactor upload component using fileUploadForm

--- client/components/Upload/index.tsx
+++ client/components/Upload/index.tsx
@@ -9,6 +9,7 @@ import { Input } from "../ui/input";
 import { quantum } from 'ldrs'
 import CancelIcon from "@/public/icon/cancelIcon";
 import { analyzeFile } from "@/services/api";
+import FileUploadForm from "./fileUploadForm";
 
 const UploadFile = () => {
   const [file, setFile] = useState<FileList | null>(null);
@@ -83,43 +84,13 @@ const UploadFile = () => {
           <h3 className="text-xl my-7 font-semibold text-white text-center">Analysing your data, stay put</h3>
         </div>
         ) : (
-          <div className="flex flex-col items-center justify-center w-full max-w-md p-6 space-y- mt-20 md:mt-0 bg-neutral-50/100 rounded-lg">
-            <form
-              onSubmit={handleSubmit}
-              className="w-full max-w-m px-1 py-6"
-              encType="multipart/form-data"
-            >
-              <h1 className="text-4xl font-bold text-center">Upload File</h1>
-              <div className="relative">
-                <UploadIcon
-                  className="absolute left-2 top-2 cursor-pointer"
-                  onClick={() => fileInputRef.current?.click()}
-                />
-                <Input
-                  className="pl-8 mt-12 cursor-pointer"
-                  type="file"
-                  ref={fileInputRef}
-                  onChange={handleFileChange}
-                  id="file"
-                />
-                {file && (
-                  <button
-                    className="absolute right-3 top-2 font-bold font-inter cursor-pointer"
-                    onClick={handleFileClear}
-                  >
-                    <CancelIcon />
-                  </button>
-                )}
-              </div>
-              <Button
-                className="w-full h-8 mt-8 text-lg bg-blue-600 hover:bg-blue-700 rounded-full"
-                disabled={!file}
-                type="submit"
-              >
-                Submit
-              </Button>
-            </form>
-          </div>
+          <FileUploadForm
+            fileInputRef={fileInputRef}
+            handleSubmit={handleSubmit}
+            handleFileChange={handleFileChange}
+            handleFileClear={handleFileClear}
+            file={file}
+          />
         )}
     </div>
   );
