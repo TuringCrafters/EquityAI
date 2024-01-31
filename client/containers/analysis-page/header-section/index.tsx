a61317b54bd69df79af620bb22f049e2824b4915
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-31 13:08:08 +0100

    delete space in equityAi

--- client/containers/analysis-page/header-section/index.tsx
+++ client/containers/analysis-page/header-section/index.tsx
@@ -5,7 +5,7 @@ export const AnalysisHeader = () => {
     <section className="mt-20 mx-20 print">
       <div className="ml-20">
         <h1 className="text-slate-950 text-4xl font-bold mb-4">
-          Pay Equity AI Analysis
+          Pay EquityAI Analysis
         </h1>
         <p className=" text-sm text-neutral-500">
           This is your report based on the data you have submited.
