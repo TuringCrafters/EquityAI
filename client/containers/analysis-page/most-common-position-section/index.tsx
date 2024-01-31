4578a0b05e77e842d49d5607d50688b559c0a771
Anastasia Kurayshevich <a.kurayshevich@gmail.com>
2024-01-31 12:11:46 +0100

    change class names

--- client/containers/analysis-page/most-common-position-section/index.tsx
+++ client/containers/analysis-page/most-common-position-section/index.tsx
@@ -11,16 +11,16 @@ export const MostCommonPosition = () => {
   const { data } = useContext(DataContext);
   return (
 
-    <section className="px-20 mx-20 mt-10 border-b-2 border-b-neutral-200 print">
+    <section className="px-20 mx-20 mt-10 border-b-2 border-b-neutral-200 print print-position">
       <h2 className="text-slate-950 text-2xl font-bold title-for-print mb-3">Your most common position</h2>
     <h3 className=" text-neutral-500 text-xl font-semibold ">
       {data?.job_title}
     </h3>
     <section className="flex flex-col items-center md:flex-row md:items-start align-middle h-3/6 justify-center my-10">
-      <div className="items-center h-full border-r-2 border-r-neutral-200 chart-for-print">
+      <div className="items-center h-full border-r-2 border-r-neutral-200 print-position__chart">
         {data?.location_details && (
           <>
-            <h3 className="ml-10 mt-5 font-semibold tracking-tighter subtitle-for-print-one">
+            <h3 className="ml-10 mt-5 font-semibold tracking-tighter print-position__chart__title">
               Salary based on Location
             </h3>
             <StaticBarChartgraph
@@ -30,10 +30,10 @@ export const MostCommonPosition = () => {
           </>
         )}
       </div>
-      <div className="items-center  max-h-100 chart-for-print">
+      <div className="items-center  max-h-100 print-position__chart">
         {data?.experience_details && (
           <>
-            <h3 className="ml-10 mt-5 mb-10 font-semibold tracking-tighter subtitle-for-print-two">
+            <h3 className="ml-10 mt-5 mb-10 font-semibold tracking-tighter print-position__chart__title-two">
               Salary based on Years of Experience
             </h3>
             <LineOfBestFitChart
