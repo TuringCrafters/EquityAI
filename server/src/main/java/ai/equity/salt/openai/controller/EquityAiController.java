0c90221e0a1bea846a81da69bd6d87bcf447f260
AnneMariel95 <a.mariel.recio@gmail.com>
2024-02-02 10:33:21 +0100

    remove unused import

--- server/src/main/java/ai/equity/salt/openai/controller/EquityAiController.java
+++ server/src/main/java/ai/equity/salt/openai/controller/EquityAiController.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.CREATED;
