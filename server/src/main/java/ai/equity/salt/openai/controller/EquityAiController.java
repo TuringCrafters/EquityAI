186ffbd267bd6362c1b9e0d62db513bf65fc9702
Ibrahim <Ibrahim@ebs.net.pk>
2024-02-06 15:09:58 +0100

    Remove unused code in EquityAiController

--- server/src/main/java/ai/equity/salt/openai/controller/EquityAiController.java
+++ server/src/main/java/ai/equity/salt/openai/controller/EquityAiController.java
@@ -1,13 +1,10 @@
 package ai.equity.salt.openai.controller;
 
 import ai.equity.salt.openai.controller.dto.EquityAiResponse;
-import ai.equity.salt.openai.controller.dto.JobDataSet;
 import ai.equity.salt.openai.service.EquityAiService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 import static org.springframework.http.HttpStatus.CREATED;
 
@@ -24,9 +21,4 @@ public class EquityAiController {
         return service.analyzeFile(file);
     }
 
-    @PostMapping("file/any")
-    @ResponseStatus(CREATED)
-    public List<JobDataSet> sendAnyFile(@RequestParam MultipartFile file) {
-        return service.readAnyFile(file);
-    }
 }
